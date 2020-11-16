530d1a37a555acf526ae257f5df42104495c955e
Rohit C <rohitblade@yahoo.com>
2020-11-15 21:26:44 -0500

    good to go

--- backend/app.py
+++ backend/app.py
@@ -20,6 +20,7 @@ class food_item(db.Model):
     tag = db.Column(db.String(20), unique = False)
     item_name = db.Column(db.String(30), unique = False)
     
+    #used for display functions later when fetching data from sql
     def __repr__(self):
         return '<Tag %r>' % self.tag + '<Food %r>' % self.item_name
 
@@ -36,23 +37,27 @@ def ExampleEndpoint():
 @app.route("/insert/tag")
 def newTag():
     input_tag = request.args['input_tag']
+    #makes sure we haven't already created the tag
     if(input_tag in valid_tags):
         raise Exception("Attempted to create tag that already exists")
     
     #no objects with that tag yet
     valid_tags[input_tag] = 0
     return "Success"
-    
+
 
 #create item with existing tag
 @app.route("/insert/item_tag")
 def insert_item():
     input_tag = request.args['input_tag']
     new_item = request.args['new_item']
+    
+    #verifies that the tag actually exists
     if(input_tag not in valid_tags):
-        raise Exception("Attempted to input tag that doesn't exists")
+        raise Exception("Attempted to input item for a tag that doesn't exists")
     
     nextFood = food_item(tag = input_tag, item_name = new_item)
+   
     db.session.add(nextFood)
     db.session.commit()
     valid_tags[input_tag] += 1
@@ -62,7 +67,10 @@ def insert_item():
 @app.route("/insert/item")
 def insert_item_only():
     new_item = request.args['new_item']
-    nextFood = food_item(tag = "", item_name = new_item)
+    
+    #"none" is used as a placeholder no tag
+    nextFood = food_item(tag = "None", item_name = new_item)
+    
     db.session.add(nextFood)
     db.session.commit()
     return "Success"
@@ -72,7 +80,19 @@ def insert_item_only():
 @app.route("/read/tag")
 def read_tag():
     input_tag = request.args['input_tag']
+    #queries all the items in the db that match the specific input tag
     return str(food_item.query.filter_by(tag = input_tag).all())
+
+#read all items with existing item name
+@app.route("/read/item_name")
+def read_item_name():
+    name = request.args['item_name']
+    #queries all the items in the db that match the specific input name
+    return str(food_item.query.filter_by(item_name = name).all())
+    
+@app.route("/read/tags")
+def read_all_tags():
+    return valid_tags
     
 #change the tag for one item
 @app.route("/update/item")
@@ -88,21 +108,17 @@ def change_tag():
     if(res is None):
         raise Exception("Attempted to update tag for item that doesn't exist")
     
+    #switches the tag and updates the db
     res.tag = new_tag
     db.session.commit()
     return "Success"
     
-#delete a item-tag
-@app.route("/delete/item")
-def delete_item():
+#delete an item-tag
+@app.route("/delete/item-tag")
+def delete_item_tag():
     tag = request.args['input_tag']
     name = request.args['item_name']
-    res = food_item.query.filter(food_item.tag == tag, food_item.item_name == name).first()
-    if(res is None):
-        raise Exception("Attempted to delete item-tag that doesn't exist")
-    db.session.delete(res)
-    db.session.commit()
-    return "Success"
+    return item_delete_helper(tag, name)
     
 #delete a tag
 @app.route("/delete/tag")
@@ -112,6 +128,22 @@ def delete_tag():
         raise Exception("Attempted to tag item that doesn't exist")
     del valid_tags[tag]
     return "Success"
+    
+#deletes an item, that does not have a tag
+@app.route("/delete/item")
+def delete_item():
+    name = request.args['input_name']
+    return item_delete_helper("None", name)
+    
+#helper function for deletes, since deleting item with no tag and item with tag has code overlap
+def item_delete_helper(tag, name):
+    res = food_item.query.filter(food_item.tag == tag, food_item.item_name == name).first()
+    if(res is None):
+        raise Exception("Attempted to delete item-tag that doesn't exist")
+    db.session.delete(res)
+    db.session.commit()
+    return "Success"
+    
 
 #returns all items in string form
 @app.route("/displayall")
